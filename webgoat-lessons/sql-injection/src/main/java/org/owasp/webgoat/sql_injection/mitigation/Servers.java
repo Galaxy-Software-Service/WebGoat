957a12e7281cc89564ed3b8bb1fc44c87023a8b6
tinaGss <136422733+tinaGss@users.noreply.github.com>
2023-11-09 10:47:26 +0800

    Update Servers.java

--- webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/sql_injection/mitigation/Servers.java
+++ webgoat-lessons/sql-injection/src/main/java/org/owasp/webgoat/sql_injection/mitigation/Servers.java
@@ -20,22 +20,20 @@
  * Source for this application is maintained at https://github.com/WebGoat/WebGoat, a repository for free software projects.
  */
 
-package org.owasp.webgoat.sql_injection.mitigation;
+package org.owasp.webgoat.lessons.sqlinjection.mitigation;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.owasp.webgoat.container.LessonDataSource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author nbaars
@@ -46,38 +44,50 @@ import java.util.List;
 @Slf4j
 public class Servers {
 
-    private final DataSource dataSource;
+  private final LessonDataSource dataSource;
 
-    @AllArgsConstructor
-    @Getter
-    private class Server {
+  @AllArgsConstructor
+  @Getter
+  private class Server {
 
-        private String id;
-        private String hostname;
-        private String ip;
-        private String mac;
-        private String status;
-        private String description;
-    }
+    private String id;
+    private String hostname;
+    private String ip;
+    private String mac;
+    private String status;
+    private String description;
+  }
 
-    public Servers(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+  public Servers(LessonDataSource dataSource) {
+    this.dataSource = dataSource;
+  }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public List<Server> sort(@RequestParam String column) throws Exception {
-        List<Server> servers = new ArrayList<>();
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseBody
+  public List<Server> sort(@RequestParam String column) throws Exception {
+    List<Server> servers = new ArrayList<>();
 
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement("select id, hostname, ip, mac, status, description from servers  where status <> 'out of order' order by " + column)) {
-            ResultSet rs = preparedStatement.executeQuery();
-            while (rs.next()) {
-                Server server = new Server(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6));
-                servers.add(server);
-            }
+    try (var connection = dataSource.getConnection()) {
+      try (var statement =
+          connection.prepareStatement(
+              "select id, hostname, ip, mac, status, description from SERVERS where status <> 'out"
+                  + " of order' order by "
+                  + column)) {
+        try (var rs = statement.executeQuery()) {
+          while (rs.next()) {
+            Server server =
+                new Server(
+                    rs.getString(1),
+                    rs.getString(2),
+                    rs.getString(3),
+                    rs.getString(4),
+                    rs.getString(5),
+                    rs.getString(6));
+            servers.add(server);
+          }
         }
-        return servers;
+      }
     }
-
+    return servers;
+  }
 }
